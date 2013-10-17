b69cfe81f9f79777819d7d4c4e8a2fdacb076b1e
Givanna <ghar1821@gmail.com>
2013-10-18 10:54:38 +1100

    fixed uniquness email and year enrolled

--- studydrone/accounts/forms.py
+++ studydrone/accounts/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import User_Profile
 
+import datetime
+
 # Safe from injection, etc.
 class UserRegistrationForm(UserCreationForm):
 	email = forms.EmailField(required = True)
@@ -13,9 +15,14 @@ class UserRegistrationForm(UserCreationForm):
 		model = User
 		fields = ('username','email','password1', 'password2','first_name','last_name',)
 
+	def clean_email(self):
+	    data = self.cleaned_data['email']
+	    if User.objects.filter(email=data).exists():
+	        raise forms.ValidationError("This email already used")
+	    return data
 	def save(self, commit=True):
 		user = super(UserRegistrationForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
+		user.email = self.clean_email()
 		user.first_name = self.cleaned_data['first_name']
 		user.last_name = self.cleaned_data['last_name']
 		
@@ -29,22 +36,8 @@ class UserProfileRegistrationForm(forms.ModelForm):
 		model = User_Profile
 		exclude = ('User_associated', 'Unikey_validated', 'Profile_picture', 'Points',)
 
-# class UserProfileRegistrationForm(forms.Form):
-# 	unikey = forms.CharField(max_length=8, required=True)
-# 	degree = forms.CharField(max_length=100)
-# 	Year_first_enrolled = forms.ChoiceField(choices=[(x,x) for x in range(2000, 2013)])
-
-# 	class Meta:
-# 		model = User_Profile
-# 		fields = ('unikey','degree','Year_first_enrolled',)
-
-# 	def save(self, commit=True):
-# 		user = super(UserProfileRegistrationForm, self).save(commit=False)
-# 		user.unikey = self.cleaned_data['unikey']
-# 		user.degree = self.cleaned_data['degree']
-# 		user.Year_first_enrolled = self.cleaned_data['Year_first_enrolled']
-
-# 		if commit:
-# 			user.save()
-
-# 		return user
+	def __init__(self, *args, **kwargs):
+		super(UserProfileRegistrationForm, self).__init__(*args, **kwargs)
+		year = datetime.datetime.now().year
+		year_choices = ( (x, str(x)) for x in range(1990,year+1) )
+		self.fields['Year_first_enrolled'] = forms.ChoiceField(choices=year_choices)
