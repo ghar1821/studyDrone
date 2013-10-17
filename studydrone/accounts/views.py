7ab80f2eef61feff4ee30288b4c9e6102dbb5047
Givanna <ghar1821@gmail.com>
2013-10-17 11:19:20 +1100

    some work done

--- studydrone/accounts/views.py
+++ studydrone/accounts/views.py
@@ -9,14 +9,20 @@ from django.shortcuts import render_to_response, redirect
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import auth
-from accounts.forms import UserRegistrationForm
+# from accounts.forms import UserRegistrationForm
 from django.core.context_processors import csrf
 
 # Added 16/10/2013 12:53 PM
-from django.contrib.auth.forms import UserCreationForm
-from django.template import RequestContext
-from django.shortcuts import render_to_response,get_object_or_404
-from django.core import urlresolvers
+# from django.contrib.auth.forms import UserCreationForm
+# from django.template import RequestContext
+# from django.shortcuts import render_to_response,get_object_or_404
+# from django.core import urlresolvers
+
+# Added 17/10/2013 9:55 AM
+from accounts.models import User_Profile
+from django.contrib.auth.models import User
+from accounts.forms import UserRegistrationForm, UserProfileRegistrationForm
+
 
 #Should this go to settings.html, or will there be another accounts home page?
 def index(request):
@@ -39,20 +45,36 @@ def delete_account(request):
 	return render(request, 'accounts/delete-account.html', {"foo": "bar"})
 
 def register(request):
-    if request.POST:
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/accounts/register-success')
+    if request.method == 'POST':
+        userForm = UserRegistrationForm(request.POST,prefix="user_form" )
+        profileForm = UserProfileRegistrationForm(request.POST, prefix="profile_form")
+        if userForm.is_valid() and profile_form.is_valid():
+            new_user = userForm.save()
+            new_profile = profileForm.save(commit=False)
+            new_profile.User_associated = new_user
+            new_profile.save()
+        return HttpResponseRedirect('/accounts/register-success')
     else:
-        form = UserRegistrationForm()
-    args = {}
-    # prevent forgery
-    args.update(csrf(request))
+        userForm = UserRegistrationForm(prefix="user_form")
+        profileForm = UserProfileRegistrationForm(prefix="profile_form")
+    return render_to_response('accounts/signup.html', {'user_form': userForm, 'profile_form': profileForm})
 
-    # empty form
-    args['form'] = form
-    return render_to_response('accounts/signup.html', args)
+
+# def register(request):
+#     if request.POST:
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/accounts/register-success')
+#     else:
+#         form = UserRegistrationForm()
+#     args = {}
+#     # prevent forgery
+#     args.update(csrf(request))
+
+#     # empty form
+#     args['form'] = form
+#     return render_to_response('accounts/signup.html', args)
 
 def register_success(request):
     return render_to_response('accounts/signup_success.html')
