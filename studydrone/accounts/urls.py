7a4802955d15160c1d65484a216eeb1bfb5a9480
Givanna <ghar1821@gmail.com>
2013-10-23 15:15:32 +1100

    fixed links to user settings

--- studydrone/accounts/urls.py
+++ studydrone/accounts/urls.py
@@ -5,15 +5,17 @@ from accounts import views
 urlpatterns = patterns('', 
 	url(r'^$', views.index),
 	url(r'^login/', views.login),
-	# url(r'^settings/', views.settings),
+	
     url(r'^points-history/', views.points_history),
     url(r'^delete-account/', views.delete_account),
     url(r'^register/', views.register),
     url(r'^register-success/', views.register_success),
-    url(r'^get/(?P<user_id>\d+)/$', views.edit_user),
-    url(r'^get/(?P<user_id>\d+)/edit-password/$', views.edit_user_password),
-    url(r'^get/(?P<user_id>\d+)/edit-profile-picture/$', views.edit_user_picture),
-    url(r'^delete/(?P<user_id>\d+)/$', views.delete_user),
+   
+    url(r'^delete-account/$', views.delete_account),
+    url(r'^delete-user/$', views.delete_user),
+    url(r'^settings/$', views.edit_user),
+    url(r'^settings/avatar/$', views.edit_user_picture),
+    url(r'^settings/password/$', views.edit_user_password),
 
 ) 
 
